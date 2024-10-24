005c75b0984b0a83ec0c5c32398fd4038f07fed4
Taben Malik <tabenjmalik@gmail.com>
2024-10-23 19:07:55 -0500

    Ensuring gnome extensions are enabled

--- gnome-setup.sh
+++ gnome-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ensure the gnome extensions are enabled
+gsettings set org.gnome.shell disable-user-extensions false
+
 # Always launch a new window/instance when selecting from the applications menu
 # I know where to find the currently running instances, so if I am going to the applications
 # menu, it is to launch a new instance
